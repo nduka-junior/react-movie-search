3519c53a22240eb52dec37efef044d15c7864495
nduka-junior <ndukajunior4@gmail.com>
2020-07-30 02:22:52 +0100

    Updates

---
+++ precache-manifest.5de4941037b77c20b2afcb1ad0c65d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24328bdbbcd52c2783f6ad4da7fe91f8",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "8ac8b5d34033ede1f176",
+    "url": "/react-movie-search/static/css/main.bc782b37.chunk.css"
+  },
+  {
+    "revision": "eb763f58d1935263f2fa",
+    "url": "/react-movie-search/static/js/2.c66c1d7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.c66c1d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac8b5d34033ede1f176",
+    "url": "/react-movie-search/static/js/main.523c3d18.chunk.js"
+  },
+  {
+    "revision": "dff6dab926eeb27f3c9e",
+    "url": "/react-movie-search/static/js/runtime-main.ada54174.js"
+  }
+]);
